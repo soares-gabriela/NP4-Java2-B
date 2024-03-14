24954f8a4aca1809f63a2e59d7d7e409b430353c
soares-gabriela <gaabyh.soares@gmail.com>
2024-03-13 22:05:49 -0300

    Conteúdo da aula do dia 13-03-2024

---
+++ NP24_JAVA2_B/src/br/com/senaisp/bauru/gabriela/secao02/ArtAscii1.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.gabriela.secao02;
+
+public class ArtAscii1 {
+
+	public static void main(String[] args) {
+System.out.println("  ████  ");
+System.out.println(" █    █ ");
+System.out.println("█ █  █ █");
+System.out.println("█      █");
+System.out.println("█ █  █ █");
+System.out.println("█  ██  █");
+System.out.println(" █    █ ");
+System.out.println("  ████  ");
+	}
+
+}
---
+++ NP24_JAVA2_B/src/br/com/senaisp/bauru/gabriela/secao02/ArtAscii2.java
@@ -0,0 +1,24 @@
+package br.com.senaisp.bauru.gabriela.secao02;
+
+public class ArtAscii2 {
+
+	public static void main(String[] args) {
+System.out.println("Sonic the Hedgehog\r\n"
+		+ "       ___------__\r\n"
+		+ " |\\__-- /\\       _-\r\n"
+		+ " |/    __      -\r\n"
+		+ " //\\  /  \\    /__\r\n"
+		+ " |  o|  0|__     --_\r\n"
+		+ " \\\\____-- __ \\   ___-\r\n"
+		+ " (@@    __/  / /_\r\n"
+		+ "    -_____---   --_\r\n"
+		+ "     //  \\ \\\\   ___-\r\n"
+		+ "   //|\\__/  \\\\  \\\r\n"
+		+ "   \\_-\\_____/  \\-\\\r\n"
+		+ "        // \\\\--\\|   -Han J. Lee-\r\n"
+		+ "   ____//  ||_\r\n"
+		+ "  /_____\\ /___\\\r\n"
+		+ "______________________");
+	}
+
+}
